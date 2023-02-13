b5684cc7295e195823cafda29341a471b312fc23
BelindaHao <hhaojue@gmail.com>
2023-02-13 19:38:14 +1100

    Init algorithms repo and add bubbleSort function

---
+++ index.ts
@@ -0,0 +1,9 @@
+let sortFunc = require("./sort/sort");
+
+let numberArr: Array<number> = [4,  19, 21, 55, 21, 6];
+let stringArr: String[] = ['H1', 'H2', 'h6', 'h8'];
+sortFunc.bubbleSort(numberArr);
+sortFunc.bubbleSort(stringArr);
+
+console.log(numberArr);
+console.log(stringArr);
---
+++ sort/sort.ts
@@ -0,0 +1,12 @@
+function bubbleSort<T>(arr:Array<T>): void{
+    for(let i=0; i<arr.length-1; i++) {
+        for(let j=i+1; j<arr.length; j++) {
+            if(arr[i] > arr[j]){
+                [arr[j], arr[i]] = [arr[i], arr[j]];
+                break;
+            }
+        }
+    }
+}
+
+module.exports = { bubbleSort }
