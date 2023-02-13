05d163c6a7131462273b94c93b7d1442963b5a94
BelindaHao <hhaojue@gmail.com>
2023-02-13 19:41:46 +1100

    remove compiled js files

--- index.js
+++
@@ -1,7 +0,0 @@
-var sortFunc = require("./sort/sort");
-var numberArr = [4, 19, 21, 55, 21, 6];
-var stringArr = ['H1', 'H2', 'h6', 'h8'];
-sortFunc.bubbleSort(numberArr);
-sortFunc.bubbleSort(stringArr);
-console.log(numberArr);
-console.log(stringArr);
