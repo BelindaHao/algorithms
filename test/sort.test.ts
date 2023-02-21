4ec1f360120dd0b8305d5387471297b62ff49211
BelindaHao <hhaojue@gmail.com>
2023-02-21 23:51:28 +1100

    Fixed test to use immutable sort funcs

--- test/sort.test.ts
+++ test/sort.test.ts
@@ -4,14 +4,14 @@ describe("test sort algorithms", () => {
     let arr:Array<number> = [4, 21, 19, 55, 21, 6];
 
     it("should bubble sort number array", () => {
-        sortFunc.bubbleSort(arr);
-        expect(arr).toEqual([4, 6, 19, 21, 21, 55]);
+        let sortedArr = sortFunc.bubbleSort(arr);
+        expect(sortedArr).toEqual([4, 6, 19, 21, 21, 55]);
     });
 
     arr = [4, 21, 19, 55, 21, 6];
     it("should selection sort number array", () => {
-        sortFunc.selectionSort(arr);
-        expect(arr).toEqual([4, 6, 19, 21, 21, 55]);
+        let sortedArr = sortFunc.selectionSort(arr);
+        expect(sortedArr).toEqual([4, 6, 19, 21, 21, 55]);
     });
 });
 
