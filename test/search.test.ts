3e80ca0dcac4965161fa594ebb876967a20adb4b
BelindaHao <hhaojue@gmail.com>
2023-03-06 21:38:31 +1100

    Added tests for searching in array of strings

--- test/search.test.ts
+++ test/search.test.ts
@@ -1,17 +1,26 @@
-const sarchFunc = require("../src/search/search")
+const searchFunc = require("../src/search/search")
 
-describe("test search algorithms", () => {
+describe("test search algorithms for numbers", () => {
     let arr:Array<number> = [4, 6, 19, 21, 21, 55];
 
     it("should return the index of array", () => {
-        let index = sarchFunc.binarySearch(arr, 19);
+        let index = searchFunc.binarySearch(arr, 19);
         expect(index).toEqual(2);
     });
 
     
     it("should return -1 if not found", () => {
-        let index = sarchFunc.binarySearch(arr, 20);
+        let index = searchFunc.binarySearch(arr, 20);
         expect(index).toEqual(-1);
+    });
+});
+
+describe("test search algorithms for strings", () => {
+    let arr:Array<string> = ["AB","Ab","aa","ab"];
+
+    it("sshould return index of array", () => {
+        let index = searchFunc.binarySearch(arr, "AB");
+        expect(index).toEqual(0);
     });
 });
 
